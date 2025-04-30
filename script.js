5d3ac7f996aa5560a4da8e09bd10d94476ac058d
Baah-Acheamfour <askmccrony@gmail.com>
2025-04-30 09:51:41 +0000

    Add operate function
    This function contains switch statement for performing the operation based on the operator clicked

--- script.js
+++ script.js
@@ -15,15 +15,11 @@ function updateDisplay() {
 // Handle number button clicks
 buttons.forEach(button => {
   button.addEventListener("click", (e) => {
-    // const value = e.target.value;
     const value = button.value;  
-    
     // If the value is a number, add it to the current input
      
     if (button.value >= '0' && button.value <= '9') {
-      let inpuvalue = parseInt(button.value)
-      currentInput += inpuvalue;
-      console.log(typeof currentInput )
+      currentInput += value;
       updateDisplay();
     }
     
@@ -34,22 +30,15 @@ buttons.forEach(button => {
         return
       } // Prevent operator without a number
       previousInput = currentInput;
-      operator = button.value;
+      operator = value;
       currentInput = "";
     }
     
     // If equals button is pressed, perform the operation
     if (button.value === "=") {
-      if (previousInput === "" || currentInput === "") return; // Ensure both numbers are entered
-      switch (operator) {
-        case '+':
-          Addition();
-          break;
+      if (previousInput === "" || currentInput === "")return; // Ensure both numbers are entered
       
-        default:
-          break;
-      }
-      // currentInput = operate(operator, parseFloat(previousInput), parseFloat(currentInput)).toString();
+      currentInput = operate(operator, parseFloat(previousInput), parseFloat(currentInput)).toString();
       updateDisplay();
       previousInput = ""; // Reset previous input
       operator = ""; // Reset operator
@@ -62,20 +51,28 @@ buttons.forEach(button => {
       operator = "";
       updateDisplay();
     }
-
-
-    function Addition(previousInput, currentInput){
-      let prev = parseFloat(previousInput); 
-      let curr = parseFloat(currentInput); 
-      let sum =prev + curr;
-      console.log(sum);
-      console.log(typeof prev)
-    }
-
   });
 });
 
 
+function operate(operator, previousInput, currentInput) {
+  switch (operator) {
+    case '+':
+      return Addition(previousInput, currentInput);
+    case '-':
+      return subtraction(previousInput, currentInput);
+    case '*':
+      return Multiplication(previousInput, currentInput);
+    case '/':
+      return division(previousInput, currentInput);
+    default:
+      return currentInput;
+  }
+}
+
+function Addition(previousInput, currentInput){
+return previousInput + currentInput
+}
 
 function subtraction(previousInput, currentInput){
   return previousInput - currentInput;
